8f9284df27942fecde47e31290d28a01e9a0f261
Sebastian <sebastian@stryczek.pl>
2023-07-21 19:42:39 +0200

    Update base url for github pages

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,8 +1,18 @@
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
+const PRODUCTION = "production";
+const STAGING = "staging";
+const DEVELOPMENT = "development";
+
 export default defineConfig(({ command, mode, ssrBuild }) => {
-  console.log(`[vite.config.js] command: ${command}, mode: ${mode}, ssrBuild: ${ssrBuild}`);
+  console.log(
+    `[vite.config.js] command: ${command}, mode: ${mode}, ssrBuild: ${ssrBuild}`,
+  );
+
+  const isProduction = mode === PRODUCTION;
+  const isStaging = mode === STAGING;
+  const isDevelopment = mode === DEVELOPMENT;
 
   return {
     plugins: [basicSsl()],
@@ -10,5 +20,6 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
       host: true,
       port: 3001,
     },
+    base: isStaging ? "/web__demo__web-ifc-three/" : "/",
   };
 });
